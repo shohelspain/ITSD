ab5680c35c0c508249bc301b9b79a050956b755c
Shohel rana <shohelranaspain@gmail.com>
2017-02-06 00:02:21 +0600

    Create dashbord.php

---
+++ dashbord.php
@@ -0,0 +1,17 @@
+<?php include 'db.php';?>
+<?php include 'auth.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Desboard</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="welcome">
+	<h1>Welcome to Dashboard..</h1>
+	<h2>This is another secure page</h2>
+	<a href="index.php">Home</a>
+	<a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
