18db5e7f5dcd7bf9b45b257722424004d61deea4
Shohel rana <shohelranaspain@gmail.com>
2017-02-06 00:06:12 +0600

    Update dashbord.php

--- dashbord.php
+++ dashbord.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 	<title>Desboard</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <div class="welcome">
